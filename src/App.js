d86e63a36f50d47691f066189a4c39114dca13de
andrebraga7 <andrestrevisan@gmail.com>
2023-06-23 14:23:03 +0000

    Install React and initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className="App-header">
+        <p>
+          Body
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
