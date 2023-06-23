1599f553d0c9f345ca2b3ddd7f028f7f6e610478
andrebraga7 <andrestrevisan@gmail.com>
2023-06-23 15:58:55 +0000

    Add header logo to landing view

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.App}>
-      <header className="App-header">
-        <p>
-          Body test
-        </p>
+      <header className={styles.Header}>
+        <img
+          src="https://res.cloudinary.com/andrebraga7/image/upload/v1687535779/logo_j8yrk1.jpg"
+          alt="where to logo"
+          className={styles.Logo}
+        ></img>
       </header>
     </div>
   );
