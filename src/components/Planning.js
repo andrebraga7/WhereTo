938d2effd8e65bc79f0056e1932350393edace3c
andrebraga7 <andrestrevisan@gmail.com>
2023-06-23 18:14:55 +0000

    Add subheading to the planning view

--- src/components/Planning.js
+++ src/components/Planning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../styles/Planning.module.css";
 import btnStyles from "../styles/Button.module.css";
 import Header from "./Header";
 
@@ -6,6 +7,7 @@ function Planning({ setCurrentView }) {
   return (
     <div>
       <Header />
+      <h2 className={styles.SubHeading}>Please enter a UK postcode to get started</h2>
       <button
         className={`${btnStyles.Button} ${btnStyles.Green}`}
         // When user clicks button currentView updates to landing and trigers component reload
