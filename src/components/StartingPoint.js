2f08ca11c546e8cfa8d2238600e31de8a8361683
andrebraga7 <andrestrevisan@gmail.com>
2023-06-23 20:50:44 +0000

    Add hanhleSubmit function to the StartingPoint component

--- src/components/StartingPoint.js
+++ src/components/StartingPoint.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/StartingPoint.module.css";
 import btnStyles from "../styles/Button.module.css";
 
 function StartingPoint({ setPostcodes }) {
+  const [startingPoint, setStartingPoint] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("added");
+    setPostcodes(startingPoint);
   };
 
   return (
@@ -13,8 +15,15 @@ function StartingPoint({ setPostcodes }) {
       <h2 className={styles.SubHeading}>
         Please enter your starting point postcode to get started.
       </h2>
+
+      {/* Simple form that takes the starting point postcode and adds it as the first intem of the postcodes array */}
       <form onSubmit={handleSubmit}>
-        <input className={styles.Input} placeholder="Enter postcode" />
+        <input
+          className={styles.Input}
+          placeholder="Enter postcode"
+          value={startingPoint}
+          onChange={(event) => setStartingPoint(event.target.value)}
+        />
         <button
           className={`${btnStyles.Button} ${btnStyles.Green}`}
           type="submit"
