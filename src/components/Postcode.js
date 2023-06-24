e286fc3bd21d4fc4e5a83ce5b4b1b6931306756d
andrebraga7 <andrestrevisan@gmail.com>
2023-06-24 14:54:57 +0000

    Add edit input that shows when user clicks on a postcode

--- src/components/Postcode.js
+++ src/components/Postcode.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Postcode.module.css";
 
 function Postcode({ postcode, index, setPostcodes, postcodes }) {
+  const [edit, setEdit] = useState(false);
+  const [newPostcode, setNewPostcode] = useState(postcode);
+
   const handleDelete = () => {
-    const newList = postcodes.filter((postcode, currentIndex) => currentIndex !== index);
+    const newList = postcodes.filter(
+      (postcode, currentIndex) => currentIndex !== index
+    );
     setPostcodes(newList);
+  };
+
+  const handleEdit = () => {
+    setPostcodes((prevPostcodes) =>
+      prevPostcodes.map((postcode, currentIndex) => {
+        return currentIndex === index ? newPostcode : postcode;
+      })
+    );
+    setEdit(false);
   };
 
   return (
@@ -12,10 +26,21 @@ function Postcode({ postcode, index, setPostcodes, postcodes }) {
       <div className={styles.Dot}>
         <i className="fa-solid fa-circle"></i>
       </div>
-      <div className={styles.PostcodeBox}>
-        <span>{postcode}</span>
-        <i className="fa-solid fa-equals"></i>
-      </div>
+      {edit ? (
+        <div className={styles.PostcodeBox} onMouseLeave={handleEdit}>
+          <input
+            name="postcode"
+            value={newPostcode}
+            onChange={(event) => setNewPostcode(event.target.value)}
+          />
+          <i className="fa-solid fa-equals"></i>
+        </div>
+      ) : (
+        <div className={styles.PostcodeBox} onClick={() => setEdit(true)}>
+          <span>{postcode}</span>
+          <i className="fa-solid fa-equals"></i>
+        </div>
+      )}
       <div className={styles.Delete} onClick={handleDelete}>
         <i className="fa-solid fa-xmark"></i>
       </div>
