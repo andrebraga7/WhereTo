7fab0f9ff6e09a31909c4c9a1b139eddba296004
andrebraga7 <andrestrevisan@gmail.com>
2023-06-24 13:21:27 +0000

    Add delete funtion to the Postcode component

--- src/components/Postcode.js
+++ src/components/Postcode.js
@@ -1,58 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/Postcode.module.css";
-import btnStyles from "../styles/Button.module.css";
 
-function Postcode({ postcodes }) {
-  const [newStop, setNewStop] = useState("");
+function Postcode({ postcode, index, setPostcodes }) {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(newStop);
+  const handleDelete = () => {
+    setPostcodes((prevPostcodes) => (
+      [...prevPostcodes.splice(index, 1)]
+    ));
   };
 
   return (
-    <div className={styles.Outer}>
-      <div className={styles.Container}>
-        {/* Iterate through all the postcodes */}
-        {postcodes.map((postcode, index) => (
-          <div key={index} className={styles.ListItem}>
-            <div className={styles.Dot}>
-              <i className="fa-solid fa-circle"></i>
-            </div>
-            <div className={styles.PostcodeBox}>
-              <span>{postcode}</span>
-              <i className="fa-solid fa-equals"></i>
-            </div>
-            <div className={styles.Delete}>
-              <i className="fa-solid fa-xmark"></i>
-            </div>
-          </div>
-        ))}
+    <div key={index} className={styles.ListItem}>
+      <div className={styles.Dot}>
+        <i className="fa-solid fa-circle"></i>
       </div>
-
-      {/* Simple form that takes the postcode and adds it to the postcodes array */}
-      <form onSubmit={handleSubmit}>
-        <div className={styles.Container}>
-          <div className={styles.ListItem}>
-            <div className={styles.Plus}>
-              <i className="fa-solid fa-plus"></i>
-            </div>
-            <input
-              className={styles.Input}
-              placeholder="Enter postcode"
-              value={newStop}
-              onChange={(event) => setNewStop(event.target.value.toUpperCase())}
-              pattern="^[A-Za-z][A-Za-z0-9]{2,3}(?:\s?[0-9][A-Za-z0-9]{2})$"
-              title="Valid postcodes must be A123 1AB or AB1 1AB"
-              maxLength={8}
-              required
-            />
-          </div>
-        </div>
-        <button className={btnStyles.ButtonInput} type="submit">
-          Add
-        </button>
-      </form>
+      <div className={styles.PostcodeBox}>
+        <span>{postcode}</span>
+        <i className="fa-solid fa-equals"></i>
+      </div>
+      <div className={styles.Delete} onClick={handleDelete}>
+        <i className="fa-solid fa-xmark"></i>
+      </div>
     </div>
   );
 }
--- src/components/PostcodesList.js
+++ src/components/PostcodesList.js
@@ -1,14 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/PostcodesList.module.css";
+import btnStyles from "../styles/Button.module.css";
 import Postcode from "./Postcode";
+import { formatPostcode } from "../utils/utils";
 
 function PostcodesList({ setPostcodes, postcodes }) {
+  const [newStop, setNewStop] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formattedPostcode = formatPostcode(newStop);
+    setPostcodes((prevPostcodes) => [...prevPostcodes, formattedPostcode]);
+  };
+
+  useEffect(() => {
+    setNewStop("");
+  }, [postcodes]);
+
   return (
     <div>
-      <h2 className={styles.SubHeading}>
-        Please add your stops
-      </h2>
-      <Postcode postcodes={postcodes} />
+      <h2 className={styles.SubHeading}>Please add your stops</h2>
+      <div>
+        <div className={styles.Container}>
+          {/* Iterate through all the postcodes */}
+          {postcodes.map((postcode, index) => (
+            <Postcode
+              key={index}
+              postcode={postcode}
+              index={index}
+              setPostcodes={setPostcodes}
+            />
+          ))}
+        </div>
+
+        {/* Simple form that takes the postcode and adds it to the postcodes array */}
+        <form onSubmit={handleSubmit}>
+          <div className={styles.Container}>
+            <div className={styles.ListItem}>
+              <div className={styles.Plus}>
+                <i className="fa-solid fa-plus"></i>
+              </div>
+              <input
+                className={styles.Input}
+                placeholder="Enter postcode"
+                value={newStop}
+                onChange={(event) =>
+                  setNewStop(event.target.value.toUpperCase())
+                }
+                pattern="^[A-Za-z][A-Za-z0-9]{2,3}(?:\s?[0-9][A-Za-z0-9]{2})$"
+                title="Valid postcodes must be A123 1AB or AB1 1AB"
+                maxLength={8}
+                required
+              />
+            </div>
+          </div>
+          <button className={btnStyles.ButtonInput} type="submit">
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
