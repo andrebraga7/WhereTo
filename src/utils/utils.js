419b4f9a8a6b8f67805103b250233430d5a137ca
andrebraga7 <andrestrevisan@gmail.com>
2023-06-25 11:04:18 +0000

    Update the formatPostcode function with toSpliced() method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,10 @@
 /** This functions takes in the postcode and formats it to have one space in the middle.
-   * It first checks to see if there is a space, if not it adds one and returns the new string.
-   */
+ * It first checks to see if there is a space, if not it adds one and returns the new string.
+ */
 export const formatPostcode = (postcode) => {
   if (postcode.at(-4) !== " ") {
     const newPostcode = postcode.split("");
-    newPostcode.splice(-3, 0, " ");
-    return newPostcode.join("");
+    return newPostcode.toSpliced(-3, 0, " ");
   }
   return postcode;
 };
