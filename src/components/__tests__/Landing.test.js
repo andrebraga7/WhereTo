eb2a3897909b5f8250904fc32870706e0910e623
Andre Braga <andrestrevisan@gmail.com>
2023-07-18 20:26:08 +0100

    Adjust tests

--- src/components/__tests__/Landing.test.js
+++ src/components/__tests__/Landing.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Landing from "../Landing";
 
-test("check Landing component loads", () => {
+test("Check Landing component loads correctly", () => {
   render(<Landing />);
+
   const title = screen.getByText("WhereTo?");
+  const button = screen.getByRole("button", { name: "Start Journey" });
+
   expect(title).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
 });
